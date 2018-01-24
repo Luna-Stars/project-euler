0f799b71ee679ecb45bb2ed308529ae216f3d473
Alex <alex.johnson3141@gmail.com>
2018-01-24 12:15:54 -0600

    added benchmark to p1

--- p001-025/p1.py
+++ p001-025/p1.py
@@ -2,4 +2,9 @@
 # Alex Johnson
 # Problem 1: Multiples of 3 and 5
 
+import time
+
+start = time.time()
 print(sum(x for x in range(1000) if x % 3 == 0 or x % 5 == 0))
+elapsed = (time.time() - start) * 1000
+print("Computed in {:.5g}ms".format(elapsed))
